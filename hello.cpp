7d7fe93c075058d8be6d6f02edddb31fc405a682
mediocre <w1rs01769@gmail.com>
2026-04-02 06:30:52 +0000

    feat:  input your name

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@
 int main(){
     std::string name;
     std::cout <<"Input your name: ";
-    std::cin <<name;
+    std::cin >> name;
 
     std::cout << "Hello, " << name << std::endl;
     return 0;
