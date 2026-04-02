90a50d1f38c25f2116fabc39e98853ea23cd8c0b
glarejezzz <w1rs01769@gmail.com>
2026-04-02 09:33:26 +0300

    fix: fixed great

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,8 @@
 int main(){
     std::string name;
     std::cout <<"Input your name: ";
-    std::cin <<name;
+    std::cin >> name;
 
-    std::cout << "Hello, " << name << std::endl;
+    std::cout << "be, " << name << std::endl;
     return 0;
 }
